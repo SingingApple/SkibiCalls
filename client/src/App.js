93187226820a0ac7462dd275c50d8861ea95d138
SingingApple <anurag.as.srivastava@gmail.com>
2021-06-06 15:02:19 +0530

    Context-Video

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { ContextProvider } from "./components/SocketContext";
 import { Typography, AppBar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
 import VideoPlayer from "./components/VideoPlayer";
@@ -32,17 +33,19 @@ function App() {
   }));
   const classes = useStyles();
   return (
-    <div className={classes.wrapper}>
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography variant="h2" align="center">
-          Skibi Chat
-        </Typography>
-      </AppBar>
-      <VideoPlayer />
-      <Options>
-        <Notifications />
-      </Options>
-    </div>
+    <ContextProvider>
+      <div className={classes.wrapper}>
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography variant="h2" align="center">
+            Skibi Chat
+          </Typography>
+        </AppBar>
+        <VideoPlayer />
+        <Options>
+          <Notifications />
+        </Options>
+      </div>
+    </ContextProvider>
   );
 }
 
